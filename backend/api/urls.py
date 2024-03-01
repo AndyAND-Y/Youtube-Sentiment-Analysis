69133d0b0ab0dd3bc6d61e85bf9d75da83e037c2
Andrei Slatinaru <andreislatinaru@gmail.com>
2024-03-01 14:07:50 +0000

    django sentiment added

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
 from . import views
+
 
 urlpatterns = [
     path("", views.default_view, name="default"),
-    path("<str:video_id>/", views.get_youtube_video_data, name="data")
+    path("<str:video_id>/", views.get_ytb_video_data, name="data"),
+    path("<str:video_id>/sentiment", views.get_ytb_comms, name="sentiment")
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,14 @@
-
 from django.http import JsonResponse
 from googleapiclient.discovery import build
+import os
+from dotenv import load_dotenv
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+load_dotenv()
+api_key = os.getenv("youtubeApiKey")
 
 
-def get_youtube_video_data(request, video_id):
-
-    api_key = 'AIzaSyBRI8cCH1CwltDRrRicNef9zxgAOsl9e5E'
+def get_ytb_video_data(request, video_id):
 
     youtube = build('youtube', 'v3', developerKey=api_key)
 
@@ -16,40 +19,58 @@ def get_youtube_video_data(request, video_id):
 
     video_data = response['items'][0]
 
-    comments = []
-    # next_page_token = None
-    # while len(comments) < 400:
-    #     comments_response = youtube.commentThreads().list(
-    #         part='snippet',
-    #         videoId=video_id,
-    #         textFormat='plainText',
-    #         maxResults=100,
-    #         pageToken=next_page_token
-    #     ).execute()
-
-    #     for item in comments_response['items']:
-    #         comment = item['snippet']['topLevelComment']['snippet']
-    #         comments.append({
-    #             'author': comment['authorDisplayName'],
-    #             'text': comment['textDisplay'],
-    #             'like_count': comment['likeCount'],
-    #             'published_at': comment['publishedAt']
-    #         })
-
-    #     next_page_token = comments_response.get('nextPageToken')
-    #     if not next_page_token:
-    #         break
-
     data = {
         'title': video_data['snippet']['title'],
         'view_count': video_data['statistics']['viewCount'],
         'thumbnail': video_data['snippet']['thumbnails']['maxres']['url'],
         'likes': video_data['statistics']['likeCount'],
         'comment_count': video_data['statistics']['commentCount'],
-        "comments": comments
     }
 
     return JsonResponse(data)
+
+
+def get_ytb_comms(request, video_id):
+
+    youtube = build('youtube', 'v3', developerKey=api_key)
+    comments = []
+    next_page_token = None
+    while len(comments) < 400:
+        comments_response = youtube.commentThreads().list(
+            part='snippet',
+            videoId=video_id,
+            textFormat='plainText',
+            maxResults=100,
+            pageToken=next_page_token
+        ).execute()
+
+        for item in comments_response['items']:
+            comment = item['snippet']['topLevelComment']['snippet']
+            comments.append({
+                'author': comment['authorDisplayName'],
+                'text': comment['textDisplay'],
+                'like_count': comment['likeCount'],
+                'published_at': comment['publishedAt']
+            })
+
+        next_page_token = comments_response.get('nextPageToken')
+        if not next_page_token:
+            break
+
+    analyzer = SentimentIntensityAnalyzer()
+
+    response = {
+        "comments": [
+            {
+                "score": {
+                    **analyzer.polarity_scores(comm['text'])
+                },
+                ** comm
+            } for comm in comments
+        ]
+    }
+
+    return JsonResponse(response)
 
 
 def default_view(request):
