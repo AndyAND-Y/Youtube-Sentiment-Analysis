a91b95ee68ef7801d8a7fb1eb1dbff6a095de85f
Andrei Slatinaru <andreislatinaru@gmail.com>
2024-03-04 00:04:17 +0000

    min-h-screen ;)

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
             <body className={poppins.className}>
                 <div className="absolute left-0 right-0 h-full w-full">
                     <div className="relative h-full w-full bg-neutral-900">
-                        <div className="relative bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]">
+                        <div className="relative min-h-screen bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]">
                             {children}
                         </div>
                     </div>
