bdb537b0cc8127d3a26280e8c8ddd43085017fcf
Andrei Slatinaru <andreislatinaru@gmail.com>
2024-04-20 13:06:09 +0100

    get stable version

--- backend/api/models/models.py
+++ backend/api/models/models.py
@@ -3,5 +3,6 @@ from .distibert import get_distibert_response
 
 MODELS = {
     "vader": get_vader_response,
-    "distibert": get_distibert_response
+    # "distibert": get_distibert_response,
+    "distibert": get_vader_response,
 }
